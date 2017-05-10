d6c14ee4f6e00af00757b6a63aba5d25e811c7dc
Valentina Pozdnyakova <pozdny@mail.ru>
2017-05-10 21:05:41 +0700

    f commit

---
+++ src/hello.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Valentina
+ * Date: 10.05.2017
+ * Time: 20:00
+ */
+namespace base;
+class Hello{
+    public function __construct(){
+        echo 'My first composer package';
+    }
+}
